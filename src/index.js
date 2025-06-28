1f1ad775f2ce02bf424d6f07663ef991c45e145c
adrinfo-doc <adrinfogamer10@gmail.com>
2025-06-27 21:58:48 -0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -28,6 +28,9 @@ app.get('/debug', (req, res) => {
   res.status(200).json({ msg: 'Debug route funcionando' });
 });
 
+app.get('/components/test', (req, res) => {
+  res.json({ message: 'Rota /components/test funcionando!' });
+});
 
 app.use('/components', componentRoutes);
 
