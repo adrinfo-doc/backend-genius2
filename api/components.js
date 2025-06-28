2f0a823e8b328b6d0688c6c629340c85e5b71004
adrinfo-doc <adrinfogamer10@gmail.com>
2025-06-28 12:18:29 -0300

    Update components.js

--- api/components.js
+++ api/components.js
@@ -1,10 +1,15 @@
+import express from 'express';
+import serverless from 'serverless-http';
 import router from '../routes/components.js';
 
-export default async function handler(req, res) {
-  try {
-    return router(req, res);
-  } catch (err) {
-    console.error('Error in /api/components:', err);
-    res.status(500).json({ error: 'Internal server error' });
-  }
-}
+const app = express();
+app.use(express.json());
+
+app.use((req, res, next) => {
+  console.log(`[API][${req.method}] ${req.url}`);
+  next();
+});
+
+app.use('/', router);
+
+export default serverless(app);
