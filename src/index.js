14bd9f9c07496160cbc74c036d893008ee487233
adrinfo-doc <adrinfogamer10@gmail.com>
2025-06-27 21:27:06 -0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ const app = express();
 app.use(cors());
 app.use(express.json({ limit: '10mb' }));
 
-app.use('/api/components', componentRoutes);
+app.use('/components', componentRoutes);
 
 app.use((err, req, res, next) => {
   console.error(err.stack);
